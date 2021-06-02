73d3788578f8bb189c7cf4593e6957ffdbeee6f2
kinguritorisu <vasilestaver@icloud.com>
2021-06-02 11:53:49 +0300

    responsive + hero Image

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <hero className="flex flex-col">
+      <hero className="flex flex-col px-4">
         <header>
           <div className="container mx-auto flex flex-row items-center py-8 py-4">
             <logo>
@@ -22,7 +22,7 @@ export default function Home() {
               </a>
             </logo>
             <nav className="flex-grow px-24">
-              <ul className="flex justify-center text-white">
+              <ul className="hidden lg:flex justify-center text-white">
                 <li className="px-2">
                   <a href="/facultati">Facultati</a>
                   <ul>
@@ -50,14 +50,14 @@ export default function Home() {
                 <li className="px-2"><a href="/facultati">Student ASEM</a></li>
               </ul>
             </nav>
-            <a href="/facultati" className="w-36 h-36 rounded-full bg-white flex items-center">
+            <a href="/facultati" className="w-36 h-36 hidden lg:flex  rounded-full bg-white flex items-center">
               <span className="px-4 text-lg text-center text-blue-800 font-extrabold transform rotate-6">
                 Devino STUDENT ASEM
               </span>
             </a>
           </div>
         </header>
-        <heroContent className="container mx-auto grid grid-cols-2 gap-4">
+        <heroContent className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-4">
           <heroleft className="flex flex-col my-8 mb-24">
             <h1 className="font-extrabold text-6xl text-white mb-6">
               Studii superioare pentru toți
@@ -69,8 +69,8 @@ export default function Home() {
               Aplica ACUM!
             </a>
           </heroleft>
-          <heroImg>
-            <Image src="/" alt="hero image" width={'auto'} height={'auto'}></Image>
+          <heroImg className="heroImg hidden lg:block">
+            {/* <Image src="/images/Bitmap.png" alt="hero image" width={'auto'} height={'auto'}></Image> */}
           </heroImg>
         </heroContent>
       </hero>
@@ -78,11 +78,11 @@ export default function Home() {
       <partners>
         <div className="container mx-auto my-28 flex flex-col items-center justify-center">
           <span className="text-sm text-gray-500">Partenerii nostri de incredere</span>
-          <ul className="grid grid-cols-4 gap-16 relative">
-            <li className="flex my-4"><a href="/facultati" style={{height: '80px', padding: '24px'}}><Image src="/images/partner-1.png" alt="partner 1" width={'auto'} height={56}></Image></a></li>
-            <li className="flex my-4"><a href="/facultati" style={{height: '80px', padding: '24px'}}><Image src="/images/partner-2.png" alt="partner 2" width={'auto'} height={56}></Image></a></li>
-            <li className="flex my-4"><a href="/facultati" style={{height: '80px', padding: '24px'}}><Image src="/images/partner-3.png" alt="partner 3" width={'auto'} height={56}></Image></a></li>
-            <li className="flex my-4"><a href="/facultati" style={{height: '80px', padding: '24px'}}><Image src="/images/partner-4.png" alt="partner 4" width={'auto'} height={56}></Image></a></li>
+          <ul className="grid grid-cols-2 md:grid-cols-4 gap-16 w-full px-4">
+            <li className="flex my-4 relative"><a href="/facultati" style={{height: '80px', padding: '24px'}}><Image src="/images/partner-1.png" alt="partner 1" layout="fill" objectFit="contain" objectPosition="center"></Image></a></li>
+            <li className="flex my-4 relative"><a href="/facultati" style={{height: '80px', padding: '24px'}}><Image src="/images/partner-2.png" alt="partner 2" layout="fill" objectFit="contain" objectPosition="center"></Image></a></li>
+            <li className="flex my-4 relative"><a href="/facultati" style={{height: '80px', padding: '24px'}}><Image src="/images/partner-3.png" alt="partner 3" layout="fill" objectFit="contain" objectPosition="center"></Image></a></li>
+            <li className="flex my-4 relative"><a href="/facultati" style={{height: '80px', padding: '24px'}}><Image src="/images/partner-4.png" alt="partner 4" layout="fill" objectFit="contain" objectPosition="center"></Image></a></li>
           </ul>
         </div>
       </partners>
@@ -91,13 +91,13 @@ export default function Home() {
         <div className="container mx-auto my-36 flex flex-col items-center justify-center">
           <h1 className="text-4xl font-extrabold mb-4">Facultati ASEM</h1>
           <description className="text-center text-gray-500 text-base">Your desire to keep forward require a university with standards as high as yours. ASEM is the first economic university in Republic of Moldova thanks to highly engaging degree programs and world-class faculties which are driven to help you be your best.</description>
-          <div className="grid grid-cols-3 gap-8 mt-16">
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-8 mt-16 px-4">
             <div>
               <a className="relative flex items-end rounded-2xl shadow-xl transform transition-all hover:shadow-2xl hover:scale-105" style={{height:"300px", overflow: 'hidden'}} href="/">
                 <div className="z-0">
                   <Image src="/images/faculty-1.png" alt="faculty 1" layout="fill" objectFit="cover" objectPosition="center"></Image>
          .       </div>
-                <span className="z-10 text-white p-8 text-2xl font-extrabold">Business si Administrarea Afacerilor</span>
+                <span className="z-10 text-white p-8 text-xl md:text-2xl font-extrabold">Business si Administrarea Afacerilor</span>
               </a>
             </div>
             <div>
@@ -105,7 +105,7 @@ export default function Home() {
                 <div className="z-0">
                   <Image src="/images/faculty-2.png" alt="faculty 2" layout="fill" objectFit="cover" objectPosition="center"></Image>
                 </div>
-                <span className="z-10 text-white p-8 text-2xl font-extrabold">Economie Generala si Drept</span>
+                <span className="z-10 text-white p-8 text-xl md:text-2xl font-extrabold">Economie Generala si Drept</span>
               </a>
             </div>
             <div>
@@ -113,7 +113,7 @@ export default function Home() {
                 <div className="z-0">
                   <Image src="/images/faculty-3.png" alt="faculty 3" layout="fill" objectFit="cover" objectPosition="center"></Image>
                 </div>
-                <span className="z-10 text-white p-8 text-2xl font-extrabold">Finante</span>
+                <span className="z-10 text-white p-8 text-xl md:text-2xl font-extrabold">Finante</span>
               </a>
             </div>
             <div>
@@ -121,7 +121,7 @@ export default function Home() {
                 <div className="z-0">
                   <Image src="/images/faculty-4.png" alt="faculty 4" layout="fill" objectFit="cover" objectPosition="center"></Image>
                 </div>
-                <span className="z-10 text-white p-8 text-2xl font-extrabold">Contabilitate</span>
+                <span className="z-10 text-white p-8 text-xl md:text-2xl font-extrabold">Contabilitate</span>
               </a>
             </div>
             <div>
@@ -129,7 +129,7 @@ export default function Home() {
                 <div className="z-0">
                   <Image src="/images/faculty-5.png" alt="faculty 5" layout="fill" objectFit="cover" objectPosition="center"></Image>
                 </div>
-                <span className="z-10 text-white p-8 text-2xl font-extrabold">Tehnologii Informationale si Statistica Economica</span>
+                <span className="z-10 text-white p-8 text-xl md:text-2xl font-extrabold">Tehnologii Informationale si Statistica Economica</span>
               </a>
             </div>
             <div>
@@ -137,7 +137,7 @@ export default function Home() {
                 <div className="z-0">
                   <Image src="/images/faculty-6.png" alt="faculty 6" layout="fill" ></Image>
                 </div>
-                <span className="z-10 text-white p-8 text-2xl font-extrabold">Relatii Economice Internationale</span>
+                <span className="z-10 text-white p-8 text-xl md:text-2xl font-extrabold">Relatii Economice Internationale</span>
               </a>
             </div>
           </div>
@@ -145,14 +145,14 @@ export default function Home() {
       </faculties>
 
       <news>
-        <div className="container mx-auto my-36">
+        <div className="container mx-auto my-36 px-4">
           <div className="flex flex-row justify-between items-end my-4">
             <h1 className="font-extrabold text-4xl">Noutati</h1>
             <a className="text-base font-medium text-blue-400" href="/">Toate noutatile</a>
           </div>
           <hr className="mb-6"></hr>
-          <div className="grid grid-cols-12 gap-6">
-            <div className="col-span-8">
+          <div className="grid grid-cols-1 md:grid-cols-12 gap-6">
+            <div className="col-span-8 md:col-span-12">
                 <div className="flex flex-col">
                   <div className="relative flex items-end rounded-2xl shadow-xl mb-6" style={{height:"500px", overflow: 'hidden'}}>
                     <Image src="/images/main-article.png" alt="news 1" layout="fill" objectFit="cover" objectPosition="center"></Image>
@@ -167,7 +167,7 @@ export default function Home() {
                   </a>
                 </div>
             </div>
-            <div className="col-span-4">
+            <div className="col-span-4 md:col-span-12">
               <div className="flex flex-row mb-6">
                 <div className="relative rounded-2xl mr-6 flex-shrink-0" style={{height: '150px', width: '150px', overflow: 'hidden'}}>
                   <Image src="/images/article-1.png" alt="news 2" layout="fill" objectFit="cover" objectPosition="center"></Image>
@@ -217,7 +217,7 @@ export default function Home() {
       </news>
 
       <about>
-        <div className="container mx-auto my-36">
+        <div className="container mx-auto my-36 px-4">
           <h1 className="text-center text-4xl font-extrabold mb-4">Despre ASEM</h1>
           <p className="text-center text-gray-500">
             ASEM este un brand academic de mare mândrie pentru cca 70 de mii de absolvenţi din 1992 încoace. În prezent, la ASEM, îşi fac studiile peste 7000 de studenţi și activează 20 de doctori habilitaţi și peste 150 de doctori în ştiinţe.
@@ -233,7 +233,7 @@ export default function Home() {
               <Image src="/images/tour.png" alt="tour" layout="fill" objectFit="cover" objectPosition="center"></Image>
             </div>
             <div className="flex flex-col flex-1 z-10">
-              <h1 className="text-white text-6xl font-extrabold">
+              <h1 className="text-white text-4xl md:text-6xl font-extrabold">
                 Turul Virtual ASEM
               </h1>
               <a className="text-white font-bold self-center flex items-center my-6 py-3 px-6 border-2 border-white rounded-xl" href="/">
@@ -248,20 +248,20 @@ export default function Home() {
       </about>
 
       <testimonials>
-        <div className="container mx-auto my-36">
+        <div className="container mx-auto my-36 px-4">
           <h1 className="text-center font-extrabold text-4xl mb-12">Ce spun studenții despre ASEM?</h1>
           <div className="relative flex items-center rounded-2xl shadow-xl my-6 grid grid-cols-12" style={{height:"500px", overflow: 'hidden'}}>
             <div className="z-0">
               <Image src="/images/testimonial-1.png" alt="testimonial 1" layout="fill" objectFit="cover" objectPosition="end"></Image>
             </div>
-            <div className="z-10 col-start-2 col-end-7 text-white">
-              <h1 className="text-3xl font-bold mb-4">
+            <div className="z-10 col-start-2 col-end-11 md:col-end-7 text-white">
+              <h1 className="text-xl md:text-3xl font-bold mb-4">
                 Cea mai bună instituție superioară
               </h1>
-              <p className="text-lg italic mb-6">
+              <p className="text-base md:text-lg italic mb-6">
                 Cu prilejul Zilei Europei, sărbătorită în fiecare an în data de 9 Mai, aducem la cunoștința întregii comunități academice, proiectele internaționale din cadrul programului Erasmus+, co-finanțate de către Comisia Europeană în derulare la Academia de Studii Economice din Moldova.
               </p>
-              <p className="text-lg italic">
+              <p className="text-base md:text-lg italic">
                 <b>Chioke Chinasa,</b><br></br>Cibernetica si Statistica Economica
               </p>
             </div>
@@ -270,14 +270,14 @@ export default function Home() {
             <div className="z-0">
               <Image src="/images/testimonial-2.png" alt="testimonial 2" layout="fill" objectFit="cover" objectPosition="end"></Image>
             </div>
-            <div className="z-10 col-start-6 col-end-12 text-white">
-              <h1 className="text-3xl font-bold mb-4">
+            <div className="z-10 col-start-2 col-end-12 md:col-start-6 text-white">
+              <h1 className="text-xl md:text-3xl font-bold mb-4">
                 Profesori excelenți și obiecte interesante
               </h1>
-              <p className="text-lg italic mb-6">
+              <p className="text-base md:text-lg italic mb-6">
                 Cu prilejul Zilei Europei, sărbătorită în fiecare an în data de 9 Mai, aducem la cunoștința întregii comunități academice, proiectele internaționale din cadrul programului Erasmus+, co-finanțate de către Comisia Europeană în derulare la Academia de Studii Economice din Moldova.
               </p>
-              <p className="text-lg italic">
+              <p className="text-base md:text-lg italic">
                 <b>Chioke Chinasa,</b><br></br>Cibernetica si Statistica Economica
               </p>
             </div>
@@ -286,7 +286,7 @@ export default function Home() {
       </testimonials>
 
       <convert>
-        <div className="container mx-auto flex flex-col my-36">
+        <div className="container mx-auto flex flex-col my-36 px-4">
           <h1 className="text-center text-4xl font-extrabold mb-4">Ești deja gata să devii student ASEM?</h1>
           <p className="text-center text-gray-500">
             Aplică ușor online la instituția superioară ASEM, iar noi avem grijă de restul
@@ -298,8 +298,8 @@ export default function Home() {
       </convert>
 
       <footer>
-        <div className="container mx-auto">
-          <div className="grid grid-cols-4 gap-12">
+        <div className="container mx-auto px-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-12">
             <ul>
               <h5 className="text-sm font-bold">Facultăți</h5>
               <li><a className="text-sm text-gray-500" href="/">Business și Administrarea Afacerilor</a></li>
